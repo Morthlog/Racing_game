2cb0df5a1c1a67b8ff24bf635f0cc3221793820a
Morthlog <iosifpetroulakis@hotmail.com>
2025-12-23 03:01:23 +0200

    refactor: enlarge level factor

---
+++ Racing game/Assets/Scripts/Level Design/MovingSpikes.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MovingSpikes : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
