e652c57664cfdeae3da091bb8e8f879241b0b8ad
Morthlog <iosifpetroulakis@hotmail.com>
2026-05-16 19:25:28 +0300

    refactor: camera bounds to work per prefab terrain

--- Racing game/Assets/Scripts/Car/CarCollision.cs
+++ Racing game/Assets/Scripts/Car/CarCollision.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CarCollision : MonoBehaviour
@@ -5,33 +6,76 @@ public class CarCollision : MonoBehaviour
     [SerializeField] VoidEventChannelSO outOfCameraLimits;
     [SerializeField] VoidEventChannelSO playerRespawned;
     private bool isOutOfBounds = false;
+    private HashSet<EntityId> bounds = new HashSet<EntityId>();
+    private string CameraBoundUser = "CameraBoundUser";
+    private string CameraBound = "CameraBound";
+    private string player = "Player";
+
+    private void Start()
+    {
+        if (!CompareTag(player)) return;
+        Transform[] allChildren = GetComponentsInChildren<Transform>(true);
+
+        foreach (Transform child in allChildren)
+        {
+            if (child == transform)
+                continue;
+
+            if (!child.CompareTag(CameraBoundUser))
+                continue;
+
+            CarCollision script = child.gameObject.AddComponent<CarCollision>();
+
+            script.outOfCameraLimits = outOfCameraLimits;
+            script.playerRespawned = playerRespawned;
+
+            Debug.Log("Added CarCollision to: " + child.name);
+        }
+        Destroy(this);
+    }
+
 
     void OnTriggerEnter(Collider other)
     {
-
         if (isOutOfBounds) return;
 
-        if (other.CompareTag("CameraBound"))
+        if (other.CompareTag(CameraBound))
         {
-            isOutOfBounds = true; 
-            outOfCameraLimits.RaiseEvent();
+            bounds.Add(other.gameObject.GetEntityId());
         }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (isOutOfBounds) return;
+
+        if (!other.CompareTag(CameraBound)) return;
+
+        if (!bounds.Remove(other.gameObject.GetEntityId())) return;
+            
+        if (bounds.Count != 0) return;
+            
+        isOutOfBounds = true;
+        outOfCameraLimits.RaiseEvent();
+        Debug.Log("Out of Bounds");
     }
 
     public void ResetOutOfBounds()
     {
         isOutOfBounds = false;
+        bounds.Clear();
     }
 
     private void OnEnable()
     {
-        playerRespawned.OnEventRaised += ResetOutOfBounds;
+        if (playerRespawned != null)
+            playerRespawned.OnEventRaised += ResetOutOfBounds;
     }
 
 
     private void OnDisable()
     {
-        playerRespawned.OnEventRaised -= ResetOutOfBounds;
-
+        if (playerRespawned != null)
+            playerRespawned.OnEventRaised -= ResetOutOfBounds;
     }
 }
