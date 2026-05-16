c051200249ae095d5ab1e185e63ed189479cab36
Morthlog <iosifpetroulakis@hotmail.com>
2026-05-16 14:52:01 +0300

    fix: obstacles damaging each other

--- Racing game/Assets/Scripts/Level Design/Damage.cs
+++ Racing game/Assets/Scripts/Level Design/Damage.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class Damage : MonoBehaviour
 {
     [SerializeField] public int damage;
+    private string topParentTag;
+    public void Start()
+    {
+        topParentTag = MainGameSceneManager.instance.GetParentTag(gameObject);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         var damageable = other.GetComponentInParent<IDamageable>();
 
         if (damageable == null) return;
-        
+        if (MainGameSceneManager.instance.GetParentTag(other.gameObject).Equals(topParentTag))
+            return;
         Debug.Log($"Hit on {other.name} for {damage}");
         damageable.TakeDamage(damage);       
     }
